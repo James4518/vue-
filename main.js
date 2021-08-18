7985c433b86165efaee91861cfa56a80023ed586
‘gcd’ <it_gcd@163.com>
2021-08-18 08:18:40 +0800

    子传父（自定义事件）

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
+const cpn = {
+    template:"#cpn",
+    data(){
+        return{
+            cars:[
+                {name:'热门'},
+                {name:'手机'},
+                {name:'电脑'},
+                {name:'电视'}
+            ] 
+        }
+    },
+    methods:{
+        zi(item){
+            this.$emit('ziclick',item)
+        }
+    }
+}
+
 new Vue({
     el:"#app",
-    data:{
-    sites:[
-        {text:'baidu'},
-        {text:'alibaba'},
-        {text:'taobao'}
-    ]}
+    components:{
+        cpn
+    },
+    methods:{
+        fuclick(item){
+            console.log('zi',item)
+        }
+    }
 })
-
-Vue.component('todo-item',{
-props:["todo"],
-template:"<li>{{todo.text}}</li>"
-})
-
