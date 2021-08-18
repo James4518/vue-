dddd55b272defa62a99169e1e9e07464eb5a2876
‘gcd’ <it_gcd@163.com>
2021-08-18 08:40:56 +0800

    父访子children

--- main.js
+++ main.js
@@ -1,18 +1,8 @@
-const cpn = {
+const cpn={
     template:"#cpn",
-    data(){
-        return{
-            cars:[
-                {name:'热门'},
-                {name:'手机'},
-                {name:'电脑'},
-                {name:'电视'}
-            ] 
-        }
-    },
     methods:{
-        zi(item){
-            this.$emit('ziclick',item)
+        showmessage(){
+            console.log("hello")
         }
     }
 }
@@ -23,8 +13,9 @@ new Vue({
         cpn
     },
     methods:{
-        fuclick(item){
-            console.log('zi',item)
+        btn(){
+            console.log(this.$children)
+            this.$children[0].showmessage()
         }
     }
 })
